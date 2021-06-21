2f3f3e1dc2fc89eb5c44dad85b74aec3074e148e
djhuthPhys <djhuth94@gmail.com>
2021-06-21 14:49:57 -0500

    Initial commit

---
+++ data_processing.py
@@ -0,0 +1,179 @@
+import os
+import glob
+import torch
+import pydicom
+
+import numpy as np
+import pandas as pd
+
+from skimage.transform import resize
+from tqdm import tqdm
+def load_labels(path):
+    """
+    Loads the train_image_level.csv and train_study_labels.csv files into a pandas dataframe. Data is then processed
+    into pytorch tensors for training a model
+    :param path: Path to directory where files are located
+    :return: bbox_labels, study_labels, example_ids
+    """
+
+    # Read in label data
+    image_data= pd.read_csv(path + '/train_image_level.csv')
+    study_data = pd.read_csv(path + '/train_study_level.csv')
+
+    # Process IDs to return
+    image_data['id'] = image_data['id'].apply(lambda x: x.split('_')[0])
+    example_ids = pd.concat([image_data['id'], image_data['StudyInstanceUID']], axis=1)
+
+    # Find maximum number of bounding boxes in a single image
+    max_boxes = max([len(image_data.label[i].split())//6 for i in range(image_data.shape[0])])
+
+    # Process image level label data into pytorch tensor format for training
+    # bbox_labels are in format (# images, maximum # boxes, # coordinates [xmin, ymin, xmax, ymax])
+    bbox_labels = torch.zeros(image_data.shape[0], max_boxes, image_data.shape[1])
+    for image in range(len(image_data.label)):
+        labels_list = [float(label) for label in image_data.label[image].split() if label.replace('.', '').isnumeric()]
+        for box in range(len(labels_list)//5):
+            x_min = labels_list[5 * box + 1]
+            y_min = labels_list[5 * box + 2]
+            x_max = labels_list[5 * box + 3]
+            y_max = labels_list[5 * box + 4]
+            bbox_coords = torch.tensor([x_min, y_min, x_max, y_max])
+            bbox_labels[image, box, :] = bbox_coords
+
+    # Process study level label data into pytorch tensor format for training
+    study_labels = torch.zeros(study_data.shape[0],study_data.shape[1] - 1)
+    for study in range(len(study_data.id)):
+        study_labels[study, 0] = study_data['Negative for Pneumonia'][study]
+        study_labels[study, 1] = study_data['Typical Appearance'][study]
+        study_labels[study, 2] = study_data['Indeterminate Appearance'][study]
+        study_labels[study, 3] = study_data['Atypical Appearance'][study]
+
+    return bbox_labels, study_labels, example_ids
+
+
+def downsample_images(path, size=512):
+    """
+    Processes all images into a size x size array for training. X and Y scale factors from resizing are saved in
+    IMAGE_NAME_scale.npy in the same directory as the image IMAGE_NAME.npy
+    :param path: Path to original images
+    :return: None
+    """
+
+    _, _, example_ids = load_labels('./data')
+
+    for image_num in range(len(example_ids))[0:10]:
+
+        study_id = example_ids['StudyInstanceUID'][image_num]
+        image_id = example_ids['id'][image_num]
+
+        image_path = glob.glob(path + '/' + study_id + '/*/' + image_id + '.dcm')[0]
+        image_data = pydicom.dcmread(image_path).pixel_array
+
+        # Resize image to size x size array and get scaling information
+        resized_image = resize(image_data, (size, size), anti_aliasing=True, preserve_range=True)
+        y_scale = image_data.shape[0]/size
+        x_scale = image_data.shape[1]/size
+        scaling = np.array([y_scale, x_scale])
+
+        # Get data set type
+        if 'train' in image_path:
+            set_type = 'train'
+        elif 'test' in image_path:
+            set_type = 'test'
+        else:
+            print('\nUnexpected data set type')
+            set_type = 'bad_type'
+
+        # Path formatting and directory creation
+        sub_directory_names = image_path.split('./data/' + set_type)[1].split('\\')[0:2]
+        sub_directory = sub_directory_names[0] + '/' + sub_directory_names[1]
+        if not os.path.exists('./data/rescaled_' + set_type + sub_directory):
+            os.makedirs('./data/rescaled_' + set_type + sub_directory)
+        rescale_path = './data/rescaled_' + set_type + '/' + image_path.split('./data/' + set_type)[1].split('.dcm')[0]
+
+        # Save image and scaling to numpy file
+        with open(rescale_path + '.npy', 'wb') as handler:
+            np.save(handler, resized_image)
+        with open(rescale_path + '_scale.npy', 'wb') as handler:
+            np.save(handler, scaling)
+        print('Saving rescaled image ' + image_id + ' from study ' + study_id)
+
+    print('Done!')
+
+    return None
+
+
+def load_images(path):
+    """
+    Loads in the .dcm images
+    :param path: Path to image files
+    :return: image_tensor: Pytorch tensor with image level data in the same order as loaded labels
+    """
+
+    _, _, example_ids = load_labels('./data')
+
+    # Get image parameters
+    num_images = len(example_ids)
+    test_image = np.load(glob.glob(path + '/' + example_ids['StudyInstanceUID'][0] + '/*/*')[0])
+    image_size = np.shape(test_image)[0]
+    image_tensor = torch.zeros((num_images, 1, image_size, image_size))
+    print('There are ' + str(num_images) + ' images in this data set of size '
+          + str(image_size) + 'X' + str(image_size) + ' pixels')
+
+    it = 0
+    for image_num in tqdm(range(len(example_ids))):
+
+        study_id = example_ids['StudyInstanceUID'][image_num]
+        image_id = example_ids['id'][image_num]
+
+        image_path = glob.glob(path + '/' + study_id + '/*/' + image_id + '.npy')
+        image = torch.from_numpy(np.load(image_path[0]))
+        image = torch.reshape(image, (1, 1, image_size, image_size))
+        image_tensor[image_num,:,:,:] = image
+        it += 1
+
+    print('\nYou\'ve loaded ' + str(it) + ' images of size '
+          + str(image_tensor.size()[2]) + 'X' + str(image_tensor.size()[3]) + ' pixels')
+
+    return image_tensor
+
+
+def load_scaling(path):
+    """
+    Loads the scaling information of the images from when they were resized with downsample_images function. The first
+    column are y scaling factors and the second are x scaling factors.
+    :param path: Path to scaling files
+    :return:
+    """
+    _, _, example_ids = load_labels('./data')
+
+    num_images = len(example_ids)
+    scaling_tensor = torch.zeros((num_images, 2))
+    print('There are ' + str(num_images) + ' scaling files in this data set')
+
+    it = 0
+    for image_num in tqdm(range(len(example_ids))):
+
+        study_id = example_ids['StudyInstanceUID'][image_num]
+        image_id = example_ids['id'][image_num]
+        scale_path = glob.glob(path + '/' + study_id + '/*/' + image_id + '_scale.npy')
+        scales = torch.from_numpy(np.load(scale_path[0]))
+        scaling_tensor[image_num,:] = scales
+        it += 1
+
+    print('\nYou\'ve loaded ' + str(it) + ' scaling values.')
+
+    return scaling_tensor
+
+def get_class_weights(study_labels):
+    """
+    Calculates the label weights used in training based on class imbalances. Ratio of population of largest class to
+    individual class populations. Largest class will have a weight of 1.
+    :param study_labels: Torch tensor containing study level labels
+    :return: class_weights: Torch tensor containing class weights
+    """
+
+    cumulative_labels = torch.sum(study_labels, dim=0)
+    class_weights = torch.max(cumulative_labels)/cumulative_labels
+
+    return class_weights
---
+++ sfrc19_ssmd_model.py
@@ -0,0 +1,102 @@
+"""
+A Single-shot Multibox Detection model for the SIIM FISABIO RSNA COVID 19 detection competition
+
+Author: Dawson Huth
+06-17-2021
+"""
+
+import torch
+import torchvision
+
+from torch import nn
+from collections import OrderedDict
+from torch.nn import functional as F
+from d2l import torch as d2l
+
+
+# Class prediction layer
+def class_predictor(num_inputs, num_anchors, num_classes):
+    return nn.Conv2d(num_inputs, num_anchors * (num_classes + 1), kernel_size=(3,3), padding=(1,1))
+
+# Bounding Box prediction layer
+def bbox_predictor(num_inputs, num_anchors):
+    return nn.Conv2d(num_inputs, num_anchors * 4, kernel_size=(3,3), padding=(1,1))
+
+# Flatten predictions for concatenation
+def flatten_preds(pred):
+    return torch.flatten(pred.permute(0,2,3,1), start_dim=1)
+
+# Concatenate predictions for loss calculation
+def concatenate_preds(preds):
+    return torch.cat([flatten_preds(p) for p in preds], dim=1)
+
+# Downsampling block
+def down_sample_block(in_channels, out_channels):
+    blk = []
+    for _ in range(2):
+        blk.append(nn.Conv2d(in_channels, out_channels, kernel_size=(3,3), padding=(1,1)))
+        blk.append(nn.BatchNorm2d(out_channels))
+        blk.append(nn.ReLU())
+        in_channels = out_channels
+    blk.append(nn.MaxPool2d(2))
+    return nn.Sequential(*blk)
+
+# Base Network
+def base_net():
+    blks = []
+    num_filters = [1, 8, 16, 32, 64]
+    for i in range(len(num_filters) - 1):
+        blks.append(down_sample_block(num_filters[i], num_filters[i + 1]))
+    return nn.Sequential(*blks)
+
+def get_blk(i):
+    if i == 0:
+        blk = base_net()
+    elif i == 1:
+        blk = down_sample_block(64, 128)
+    elif i == 4:
+        blk = nn.AdaptiveMaxPool2d((1, 1))
+    else:
+        blk = down_sample_block(128, 128)
+    return blk
+
+# Forward prop function for blocks
+def blk_forward(X, blk, size, ratio, class_predictor, bbox_predictor):
+    Y = blk(X)
+    anchors = d2l.multibox_prior(Y, sizes=size, ratios=ratio)
+    class_preds = class_predictor(Y)
+    bbox_preds = bbox_predictor(Y)
+    return (Y, anchors, class_preds, bbox_preds)
+
+# Some global variables
+sizes = [[0.2, 0.272], [0.37, 0.447], [0.54, 0.619], [0.71, 0.79],
+         [0.88, 0.961]]
+ratios = [[1, 2, 0.5]] * 5
+num_anchors = len(sizes[0]) + len(ratios[0]) - 1
+
+# Copied SSD model from 'Dive into Deep Learning
+class TinySSD(nn.Module):
+    def __init__(self, num_classes, **kwargs):
+        super(TinySSD, self).__init__(**kwargs)
+        self.num_classes = num_classes
+        idx_to_in_channels = [64, 128, 128, 128, 128]
+        for i in range(5):
+            setattr(self, f'blk_{i}', get_blk(i))
+            setattr(
+                self, f'cls_{i}',
+                class_predictor(idx_to_in_channels[i], num_anchors, num_classes))
+            setattr(self, f'bbox_{i}',
+                    bbox_predictor(idx_to_in_channels[i], num_anchors))
+
+    def forward(self, X):
+        anchors, class_preds, bbox_preds = [None] * 5, [None] * 5, [None] * 5
+        for i in range(5):
+            X, anchors[i], class_preds[i], bbox_preds[i]= blk_forward(
+                X, getattr(self, f'blk_{i}'), sizes[i], ratios[i],
+                getattr(self, f'cls_{i}'), getattr(self, f'bbox_{i}'))
+        anchors = torch.cat(anchors, dim=1)
+        class_preds = concatenate_preds(class_preds)
+        class_preds = class_preds.reshape(class_preds.shape[0], -1,
+                                          self.num_classes + 1)
+        bbox_preds = concatenate_preds(bbox_preds)
+        return anchors, class_preds, bbox_preds

f259218aa8f8d26e0383446177676a9b2d228230
djhuthPhys <djhuth94@gmail.com>
2021-06-22 16:17:36 -0500

    Added scaled feature extraction, detector head class, and began the forward pass function
    Signed-off-by: djhuthPhys <djhuth94@gmail.com>

--- sfrc19_ssmd_model.py
+++ sfrc19_ssmd_model.py
@@ -6,14 +6,13 @@ Author: Dawson Huth
 """
 
 import torch
-import torchvision
 
 from torch import nn
 from collections import OrderedDict
-from torch.nn import functional as F
 from d2l import torch as d2l
 
 
+# Base model classes and base model definition function
 class base_layer(nn.Module):
     """
     Defines the basic residual layer of the base SSD network
@@ -127,6 +126,135 @@ class base_network(nn.Module):
 def base_model(block_channels, block_depths):
     return base_network(block_channels, block_depths)
 
+
+# Scaled feature extraction
+class scaling_layer(nn.Module):
+    """
+    Defines the layer for extracting features at a new scale from the base network output
+    """
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
+        """
+
+        :param in_channels: number of input channels
+        :param out_channels: number of output channels
+        :param args:
+        :param kwargs:
+        """
+        super(scaling_layer, self).__init__()
+        self.in_channels, self.out_channels = in_channels, out_channels
+
+        self.conv = nn.Sequential(OrderedDict(
+            {
+                'conv': nn.Conv2d(in_channels, out_channels, kernel_size=(3,3), padding=(1,1), *args, **kwargs),
+
+                'norm': nn.BatchNorm2d(out_channels),
+
+                'relu': nn.ReLU()
+            }
+        ))
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class scaling_block(nn.Module):
+    """
+    Defines the blocks for scaling features. Most blocks will usually only be one layer deep but this is defined for the
+    sake of customizability
+    """
+    def __init__(self, in_channels, out_channels, layer=scaling_layer, block_depth=1, *args, **kwargs):
+        """"
+
+        :param in_channels: number of in channels (only used by first convolution layer in block)
+        :param out_channels: number of out channels (used by the rest of the layers)
+        :param layer: defines the layer to use when building the block
+        :param block_depth: defines the number of layers the block is built with
+        :param args:
+        :param kwargs:
+        """
+        super(scaling_block, self).__init__()
+
+        self.block = nn.Sequential(
+            layer(in_channels, out_channels, *args, **kwargs),
+            *[layer(out_channels, out_channels, *args, **kwargs)
+              for _ in range(block_depth-1)]
+        )
+
+        self.pooling = nn.MaxPool2d(2)
+
+    def forward(self, x):
+        for layer in self.block:
+            x = layer(x)
+            x = self.pooling(x)
+        return x
+
+
+# Detection head
+class detection_head(nn.Module):
+    """
+    Defines the detection head layers
+    """
+    def __init__(self, in_channels, num_anchors, num_classes):
+        """
+
+        :param in_channels: number of channels output by corresponding feature extraction layer
+        :param num_anchors: number of anchor boxes generated per pixel
+        :param num_classes: number of classes for the classification problem
+        """
+        super(detection_head, self).__init__()
+
+        self.in_channels, self.num_anchors, self.num_classes = in_channels, num_anchors, num_classes
+
+        self.class_predictor = nn.Conv2d(in_channels, num_anchors * (num_classes + 1), kernel_size=(3,3), padding=(1,1))
+
+        self.bbox_predictor = nn.Conv2d(in_channels, num_anchors * 4, kernel_size=(3,3), padding=(1,1))
+
+    def forward(self, x):
+        classes = self.class_predictor(x)
+        bboxes = self.bbox_predictor(x)
+        return classes, bboxes
+
+
+# Full forward pass of the SSD network
+def SSD_forward(base, scale_depths, X):
+    """
+    The full forward pass through the SSD network. Outputs class confidence scores and bbox predictions
+    :param base: The base model
+    :param X: Input data to model
+    :return: conf_scores, bbox_preds
+    """
+    # Anchor box variables
+    scales = [0.75, 0.66, 0.5, 0.33, 0.25, 0.1]
+    ratios = [1, 2, 0.5, 0.25]
+    num_classes = 4
+    num_anchors = len(scales) + len(ratios) - 1
+
+
+    # Pass through base model and generate anchor boxes
+    base_output = base(X)
+    ####################### Generate anchor boxes
+    image_height = base_output.size()[2]
+    image_width = base_output.size()[3]
+    bbox_scale = torch.tensor((image_width, image_height, image_width, image_height))
+    base_anchors = d2l.multibox_prior(base_output, sizes=scales, ratios=ratios)
+    scaled_anchors = base_anchors * bbox_scale
+    #######################
+    base_classes, base_bboxes = detection_head(base_output.size()[1], num_anchors, num_classes)(base_output)
+
+    print(base_output.size())
+    print(base_anchors.size())
+    print(scaled_anchors.size())
+    print(base_classes.size())
+    print(base_bboxes.size())
+
+    # Pass outputs from base model through scaled feature extraction
+
+
+    return None
+
+
+
+########################################################################################################################
 # Class prediction layer
 def class_predictor(num_inputs, num_anchors, num_classes):
     return nn.Conv2d(num_inputs, num_anchors * (num_classes + 1), kernel_size=(3,3), padding=(1,1))
@@ -143,36 +271,6 @@ def flatten_preds(pred):
 def concatenate_preds(preds):
     return torch.cat([flatten_preds(p) for p in preds], dim=1)
 
-# Downsampling block
-def down_sample_block(in_channels, out_channels):
-    blk = []
-    for _ in range(2):
-        blk.append(nn.Conv2d(in_channels, out_channels, kernel_size=(3,3), padding=(1,1)))
-        blk.append(nn.BatchNorm2d(out_channels))
-        blk.append(nn.ReLU())
-        in_channels = out_channels
-    blk.append(nn.MaxPool2d(2))
-    return nn.Sequential(*blk)
-
-# Base Network
-def base_net():
-    blks = []
-    num_filters = [1, 8, 16, 32, 64]
-    for i in range(len(num_filters) - 1):
-        blks.append(down_sample_block(num_filters[i], num_filters[i + 1]))
-    return nn.Sequential(*blks)
-
-def get_blk(i):
-    if i == 0:
-        blk = base_net()
-    elif i == 1:
-        blk = down_sample_block(64, 128)
-    elif i == 4:
-        blk = nn.AdaptiveMaxPool2d((1, 1))
-    else:
-        blk = down_sample_block(128, 128)
-    return blk
-
 # Forward prop function for blocks
 def blk_forward(X, blk, size, ratio, class_predictor, bbox_predictor):
     Y = blk(X)
@@ -180,36 +278,3 @@ def blk_forward(X, blk, size, ratio, class_predictor, bbox_predictor):
     class_preds = class_predictor(Y)
     bbox_preds = bbox_predictor(Y)
     return (Y, anchors, class_preds, bbox_preds)
-
-# Some global variables
-sizes = [[0.2, 0.272], [0.37, 0.447], [0.54, 0.619], [0.71, 0.79],
-         [0.88, 0.961]]
-ratios = [[1, 2, 0.5]] * 5
-num_anchors = len(sizes[0]) + len(ratios[0]) - 1
-
-# Copied SSD model from 'Dive into Deep Learning
-class TinySSD(nn.Module):
-    def __init__(self, num_classes, **kwargs):
-        super(TinySSD, self).__init__(**kwargs)
-        self.num_classes = num_classes
-        idx_to_in_channels = [64, 128, 128, 128, 128]
-        for i in range(5):
-            setattr(self, f'blk_{i}', get_blk(i))
-            setattr(
-                self, f'cls_{i}',
-                class_predictor(idx_to_in_channels[i], num_anchors, num_classes))
-            setattr(self, f'bbox_{i}',
-                    bbox_predictor(idx_to_in_channels[i], num_anchors))
-
-    def forward(self, X):
-        anchors, class_preds, bbox_preds = [None] * 5, [None] * 5, [None] * 5
-        for i in range(5):
-            X, anchors[i], class_preds[i], bbox_preds[i]= blk_forward(
-                X, getattr(self, f'blk_{i}'), sizes[i], ratios[i],
-                getattr(self, f'cls_{i}'), getattr(self, f'bbox_{i}'))
-        anchors = torch.cat(anchors, dim=1)
-        class_preds = concatenate_preds(class_preds)
-        class_preds = class_preds.reshape(class_preds.shape[0], -1,
-                                          self.num_classes + 1)
-        bbox_preds = concatenate_preds(bbox_preds)
-        return anchors, class_preds, bbox_preds
